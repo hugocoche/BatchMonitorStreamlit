f55bf4c2018a516e4e82739a378d204f62f6c5af
Hugo COCHEREAU <hugo.cochereau@hotmail.com>
2024-05-03 17:25:17 +0200

    rtytyu

--- pages/1_Demand_construction.py
+++ pages/1_Demand_construction.py
@@ -124,7 +124,6 @@ else:
                 df = pd.DataFrame(pd.read_csv(uploaded_file, index_col=0))
             elif file_extension == ".xlsx":
                 df = pd.DataFrame(pd.read_excel(uploaded_file, index_col=0))
-
             if b1.button("construct demand"):
                 for row in df.itertuples():
                     Item_Name, Item_Quantity_min, Item_Quantity_max = (
@@ -168,39 +167,68 @@ else:
         if st.button("construct demand"):
             if uploaded_file is not None:
                 item_lists = pd.read_json(uploaded_file)
-                item_list_pd = pd.DataFrame(
-                    item_lists, columns=["Item Quantity min", "Item Quantity max"]
+                df = pd.DataFrame(
+                    item_lists,
+                    columns=["Item name", "Item Quantity min", "Item Quantity max"],
                 )
-                item_list_pd.index = [
-                    item_list["name"] for item_list in item_lists["items"]
-                ]
-                item_list_pd["Item Quantity min"] = [
+                df.index = [item_list["name"] for item_list in item_lists["items"]]
+                df["Item Quantity min"] = [
                     item_list["minimum_quantity"] for item_list in item_lists["items"]
                 ]
-                item_list_pd["Item Quantity max"] = [
+                df["Item Quantity max"] = [
                     item_list["maximum_quantity"] for item_list in item_lists["items"]
                 ]
-                st.session_state["Demand_list"] = item_list_pd
+                st.session_state["Demand_list"] = df
             else:
                 st.write("Please upload a file")
+            for row in df.itertuples():
+                Item_Name, Item_Quantity_min, Item_Quantity_max = (
+                    row
+                    if len(row) == 3
+                    else [
+                        row[0],
+                        row[1],
+                        round(float("{:.2e}".format(1.797e308)), 2),
+                    ]
+                )
+                if Item_Name not in st.session_state["deja_vu"]:
+                    if "Demand_list" not in st.session_state or not isinstance(
+                        st.session_state["Demand_list"], ItemListRequest
+                    ):
+                        st.session_state["Demand_list"] = ItemListRequest(
+                            [
+                                ItemRequest(
+                                    Item_Name,
+                                    Item_Quantity_min,
+                                    Item_Quantity_max,
+                                )
+                            ]
+                        )
+                        st.session_state["deja_vu"].append(Item_Name)
+                    else:
+                        st.session_state["Demand_list"].items.append(
+                            ItemRequest(
+                                Item_Name,
+                                Item_Quantity_min,
+                                Item_Quantity_max,
+                            )
+                        )
+                        st.session_state["deja_vu"].append(Item_Name)
+                else:
+                    st.write("Item already in the list")
 
-if not isinstance(st.session_state["Demand_list"], pd.DataFrame):
-    if (
-        st.session_state["Demand_list"] is not None
-        and "Demand_list" in st.session_state
-    ):
-        for item in st.session_state["Demand_list"].items:
-            demand_info = pd.DataFrame(
-                {
-                    "Item Name": [item.name],
-                    "Item Quantity min": [item.minimum_quantity],
-                    "Item Quantity max": [item.maximum_quantity],
-                }
-            )
-            demand_info = demand_info.set_index("Item Name")
-            st.table(demand_info)
-else:
-    st.table(st.session_state["Demand_list"])
+if st.session_state["Demand_list"] is not None and "Demand_list" in st.session_state:
+    for item in st.session_state["Demand_list"].items:
+        demand_info = pd.DataFrame(
+            {
+                "Item Name": [item.name],
+                "Item Quantity min": [item.minimum_quantity],
+                "Item Quantity max": [item.maximum_quantity],
+            }
+        )
+        demand_info = demand_info.set_index("Item Name")
+        st.table(demand_info)
+
 
 if st.sidebar.button("Clear demand"):
     st.session_state["deja_vu"] = []
