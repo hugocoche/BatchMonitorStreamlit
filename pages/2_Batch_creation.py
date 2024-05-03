04690e7f5604e0524c017a1cc09a4e4740018965
Hugo COCHEREAU <hugo.cochereau@hotmail.com>
2024-05-03 21:01:22 +0200

    dsfdgfhg

--- pages/2_Batch_creation.py
+++ pages/2_Batch_creation.py
@@ -200,9 +200,7 @@ else:
         if uploaded_file is not None:
             json_df = pd.read_json(uploaded_file)
             df = create_df_from_json(json_df)
-            if df is not None:
-                st.table(df)
-    st.table(df)
+
     if uploaded_file is not None:
         for Numbers in df.iloc[:-2].values.tolist():
             if not all(isinstance(x, (int, float)) for x in Numbers):
